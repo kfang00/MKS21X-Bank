139fad9695fb95e2438bf0e1aa03ea2f572b140f
Kayla Fang <kfang00@stuy.edu>
2018-10-04 01:56:21 -0400

    adding test cases

---
+++ Driver.java
@@ -0,0 +1,17 @@
+public class Driver {
+	public static void main(String[]args) {
+		BankAccount a = new BankAccount(10024, 2352.50, "heyheyhey");
+		if(a.withdraw(1000000) ){
+             		System.out.println("Withdrawal success!");
+       		}
+		else{
+            		System.out.println("Withdrawal failure");
+		}
+		if(a.deposit(10000000) ){
+             		System.out.println("Deposit success!");
+       		}
+		else{
+            		System.out.println("Deposit failure");
+		}
+	}
+}
